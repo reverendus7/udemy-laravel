9e0aef670da726373398877ef6075063e7f1568f
Reverendus7 <i.secci87@gmail.com>
2023-03-15 09:21:44 +0100

    feat: events and event listeners

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\OurExampleEvent;
 use App\Models\Follow;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -93,6 +94,10 @@ class UserController extends Controller
     }
 
     public function logout() {
+        event(new OurExampleEvent([
+            'username' => auth()->user()->username,
+            'action' => 'logout'
+        ]));
         auth()->logout();
         return redirect('/')->with('success', 'You are now logged out.');;
     }
@@ -121,6 +126,10 @@ class UserController extends Controller
             'password' => $incomingFields['loginpassword'],
         ])) {
             $request->session()->regenerate();
+            event(new OurExampleEvent([
+                'username' => auth()->user()->username,
+                'action' => 'login'
+            ]));
             return redirect('/')->with('success', 'You have successfully logged in.');
         } else {
             return redirect('/')->with('failure', 'Invalid login.');;
